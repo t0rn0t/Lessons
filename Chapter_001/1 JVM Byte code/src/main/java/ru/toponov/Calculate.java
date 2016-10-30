a3bd2a1d2696a9c176427482808de5d19a2017e5
t0rn0t <toponovs@gmail.com>
2016-10-31 01:24:15 +0700

    Change condition

--- Chapter_001/1 JVM Byte code/src/main/java/ru/toponov/Calculate.java
+++ Chapter_001/1 JVM Byte code/src/main/java/ru/toponov/Calculate.java
@@ -3,11 +3,11 @@ package toponov;
 public class Calculate {
 	boolean flag = true;
 	public String echo(String value){
-		return String.format("dsd %s %s %s", value, value, value);
+		return String.format("%s %s %s", value, value, value);
 	}
 	public static void main(String[] args){
 		Calculate calc = new Calculate();
-		if (flag)
+		if (!flag)
 			System.out.println(calc.echo("Hello!!!!!"));
 
 	}
