c8eaef12f0988ea5922c726e01a2351b1302e53e
t0rn0t <toponovs@gmail.com>
2016-10-30 23:29:04 +0700

    Next commit

--- Chapter_001/1 JVM Byte code/src/main/java/ru/toponov/Calculate.java
+++ Chapter_001/1 JVM Byte code/src/main/java/ru/toponov/Calculate.java
@@ -2,10 +2,10 @@ package toponov;
 
 public class Calculate {
 	public String echo(String value){
-		return String.format("%s %s %s", value, value, value);
+		return String.format("dsd %s %s %s", value, value, value);
 	}
 	public static void main(String[] args){
 		Calculate calc = new Calculate();
-		System.out.println(calc.echo("Hello!!!!"));
+		System.out.println(calc.echo("Hello!!!!!"));
 	}
 }
