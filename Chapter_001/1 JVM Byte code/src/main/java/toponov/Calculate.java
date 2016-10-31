0a66d03ef582c5349130edab052795d26b2a77ca
t0rn0t <toponovs@gmail.com>
2016-10-31 08:59:35 +0700

    added maven

--- Chapter_001/1 JVM Byte code/src/main/java/toponov/Calculate.java
+++ Chapter_001/1 JVM Byte code/src/main/java/toponov/Calculate.java
@@ -1,14 +1,11 @@
 package toponov;
 
 public class Calculate {
-	boolean flag = true;
 	public String echo(String value){
 		return String.format("%s %s %s", value, value, value);
 	}
 	public static void main(String[] args){
 		Calculate calc = new Calculate();
-		if (!flag)
-			System.out.println(calc.echo("Hello!!!!!"));
-
+		System.out.println(calc.echo("Hello!"));
 	}
 }
