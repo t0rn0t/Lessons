2b705b1694756640e75ecb46fe1a500bb13da4d0
t0rn0t <toponovs@gmail.com>
2016-10-31 16:58:06 +0700

    maven modules test

---
+++ Chapter_001/src/test/java/toponov/CalculateTest.java
@@ -0,0 +1,20 @@
+package toponov;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+	@Test
+	public void whenSetStopInEchoThenReturnThreeStops(){
+		Calculate calc = new Calculate();
+		String result = calc.echo("stop");
+		assertThat(result, is("stop stop stop"));
+	}
+	@Test
+	public void whenSetNULLInEchoThenReturnTwoSpaces(){
+		Calculate calc = new Calculate();
+		String result = calc.echo(null);
+		assertThat(result, is("null null null"));
+	}
+}
